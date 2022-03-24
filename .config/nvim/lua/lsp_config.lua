e22bb04d1a9e27edf189dabcdf81efc35dc3a417
waxcoin <waxcoin@hotmail.com>
2022-03-24 16:52:17 +0100

    update

--- .config/nvim/lua/lsp_config.lua
+++ .config/nvim/lua/lsp_config.lua
@@ -82,7 +82,7 @@ local function lsp_keymaps(bufnr)
 end
 
 M.on_attach = function(client, bufnr)
-  if client.name == "tsserver" then
+  if client.name == "ccls" then
     client.resolved_capabilities.document_formatting = false
   end
   lsp_keymaps(bufnr)
