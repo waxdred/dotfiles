538eb1884a5a1dc02fda915fd71ec994fb56b7d3
waxcoin <waxcoin@hotmail.com>
2022-04-30 21:00:38 +0200

    feat: update plugin lsp

--- .config/nvim/lua/plugin.lua
+++ .config/nvim/lua/plugin.lua
@@ -22,6 +22,7 @@ local use = packer.use
 packer.reset()
 
 packer.startup(function()
+	use 'glepnir/lspsaga.nvim'
 	use 'airblade/vim-gitgutter'
 	use 'RishabhRD/popfix'
 	use 'RishabhRD/nvim-cheat.sh'
