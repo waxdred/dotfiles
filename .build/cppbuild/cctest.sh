e7a8282017bb05ddd297df6b24174ccdaa88c941
waxcoin <waxcoin@hotmail.com>
2022-05-26 23:09:37 +0200

    feat: update script ccmake and cctest

--- .build/cppbuild/cctest.sh
+++ .build/cppbuild/cctest.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
+FILE=../compile_commands.json
 cd ./build
-cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 .. && make && ctest 
+if [ -f "$FILE" ]; then	
+	make && ctest 
+else
+	cmake -DCMAKE_EXPORT_COMPILE_COMMANDS=1 .. && make && ctest 
+	mv compile_commands.json ../. 2> /dev/null
+fi
+
 if [ $# -eq 1 ]
 then
 	cat Testing/Temporary/LastTest.log
