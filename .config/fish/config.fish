b1923fb581716312a73aa24047b966c6c5cccde3
waxcoin <waxcoin@hotmail.com>
2023-12-14 09:03:30 +0100

    Config fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,7 +2,7 @@ alias 42="cd ~/code/42/$1"
 alias sfish "source ~/.dotfiles/.config/fish/config.fish"
 alias stmux "source ~/.dotfiles/.tmux.conf"
 alias vig "vi +G"
-alias vi "nvim"
+alias vi nvim
 alias touch2 "~/.dotfiles/.config/fish/touch2 $1"
 alias chat "~/code/Term_ChatGPT/bin/chatGPT"
 alias code "cd ~/code"
@@ -20,10 +20,10 @@ alias .....="cd ../../../.."
 
 
 if type -q exa
-	alias ls "exa -g --icons"
-	alias ll "exa -l -g --icons"
-	alias lla "exa -la -g --icons"
-	alias lt "ll --tree --level=3 -a --ignore-glob='\.*'"
+    alias ls "exa -g --icons"
+    alias ll "exa -l -g --icons"
+    alias lla "exa -la -g --icons"
+    alias lt "ll --tree --level=3 -a --ignore-glob='\.*'"
 end
 
 export VISUAL=/usr/local/bin/nvim
@@ -52,7 +52,7 @@ alias 42="cd ~/code/42/$1"
 alias sfish "source ~/.dotfiles/.config/fish/config.fish"
 alias stmux "source ~/.dotfiles/.tmux.conf"
 alias vig "vi +G"
-alias vi "nvim"
+alias vi nvim
 alias touch2 "~/.dotfiles/.config/fish/touch2 $1"
 alias chat "~/code/Term_ChatGPT/bin/chatGPT"
 alias code "cd ~/code"
@@ -69,10 +69,10 @@ alias .....="cd ../../../.."
 
 
 if type -q exa
-	alias ls "exa -g --icons"
-	alias ll "exa -l -g --icons"
-	alias lla "exa -la -g --icons"
-	alias lt "ll --tree --level=3 -a --ignore-glob='\.*'"
+    alias ls "exa -g --icons"
+    alias ll "exa -l -g --icons"
+    alias lla "exa -la -g --icons"
+    alias lt "ll --tree --level=3 -a --ignore-glob='\.*'"
 end
 
 export VISUAL=/usr/local/bin/nvim
@@ -80,70 +80,75 @@ export EDITOR=/usr/local/bin/nvim
 export SHELL=/opt/local/bin/fish
 export OPENAI_API_KEY="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"
 export NVM_DIR=~/.nvm
+
+# Go
+set -g GOPATH $HOME/go
+set -gx PATH $GOPATH/bin $PATH
+
 set PATH $PATH ~/code/Go/GoHotReload/
 
 function dotfile
-    gum style\
-        --border normal\
-        --margin "1"\
-        --border-foreground "$GIT_COLOR"\
+    gum style \
+        --border normal \
+        --margin 1 \
+        --border-foreground "$GIT_COLOR" \
         " Dotfile Manager "
     set choose (gum choose --no-limit "Nvim" "Fish" "Config" "Plugin" "Lsp" "HotConfig")
     switch $choose
-        case "Nvim"
+        case Nvim
             cd ~/.dotfiles/.config/nvim
-        case "Fish"
+        case Fish
             cd ~/.dotfiles/.config/fish
-        case "Config"
+        case Config
             cd ~/.dotfiles/.config/
-        case "Plugin"
+        case Plugin
             vi ~/.dotfiles/.config/nvim/lua/plugin.lua
-        case "Lsp"
+        case Lsp
             cd ~/.dotfiles/.config/nvim/lua/lsp/
-        case "HotConfig"
+        case HotConfig
             vi ~/code/Go/GoHotReload/config/config.yml
     end
 end
 
 function export_env_vars
-for line in (cat .env)
-    if test (count (string split "=" $line)) -eq 2
-        set -l key_value (string split "=" $line)
-        set -gx $key_value[1] $key_value[2]
+    for line in (cat .env)
+        if test (count (string split "=" $line)) -eq 2
+            set -l key_value (string split "=" $line)
+            set -gx $key_value[1] $key_value[2]
+        end
     end
-end
 end
 
 function dotfile
-    gum style\
-        --border normal\
-        --margin "1"\
-        --border-foreground "$GIT_COLOR"\
+    gum style \
+        --border normal \
+        --margin 1 \
+        --border-foreground "$GIT_COLOR" \
         " Dotfile Manager "
     set choose (gum choose --no-limit "Nvim" "Fish" "Config" "Plugin" "Lsp" "HotConfig")
     switch $choose
-        case "Nvim"
+        case Nvim
             cd ~/.dotfiles/.config/nvim
-        case "Fish"
+        case Fish
             cd ~/.dotfiles/.config/fish
-        case "Config"
+        case Config
             cd ~/.dotfiles/.config/
-        case "Plugin"
+        case Plugin
             vi ~/.dotfiles/.config/nvim/lua/plugin.lua
-        case "Lsp"
+        case Lsp
             cd ~/.dotfiles/.config/nvim/lua/lsp/
-        case "HotConfig"
+        case HotConfig
             vi ~/code/Go/GoHotReload/config/config.yml
     end
 end
 
 function export_env_vars
-for line in (cat .env)
-    if test (count (string split "=" $line)) -eq 2
-        set -l key_value (string split "=" $line)
-        set -gx $key_value[1] $key_value[2]
+    for line in (cat .env)
+        if test (count (string split "=" $line)) -eq 2
+            set -l key_value (string split "=" $line)
+            set -gx $key_value[1] $key_value[2]
+        end
     end
-end
 end
 
 # set theme_color_scheme solarized
