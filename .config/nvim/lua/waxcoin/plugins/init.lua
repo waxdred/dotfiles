71fa3960a3775c30140a38d884f690a3acc28432
waxcoin <waxcoin@hotmail.com>
2023-12-15 11:27:51 +0100

    Add nvim_telescope

--- .config/nvim/lua/waxcoin/plugins/init.lua
+++ .config/nvim/lua/waxcoin/plugins/init.lua
@@ -1,3 +1,10 @@
 return{
-
+  "telescope.nvim",
+    dependencies = {
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+      },
+      "nvim-telescope/telescope-file-browser.nvim",
+    },
 }
