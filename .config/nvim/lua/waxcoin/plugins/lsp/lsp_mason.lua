abb6fe8f4f7edfe803eedd80a661817a5197f94e
waxcoin <waxcoin@hotmail.com>
2024-01-10 18:56:55 +0100

    remove formatting python

--- .config/nvim/lua/waxcoin/plugins/lsp/lsp_mason.lua
+++ .config/nvim/lua/waxcoin/plugins/lsp/lsp_mason.lua
@@ -52,8 +52,6 @@ return {
 			ensure_installed = {
 				"prettier", -- prettier formatter
 				"stylua", -- lua formatter
-				"isort", -- python formatter
-				"black", -- python formatter
 				"pylint", -- python linter
 				"eslint_d", -- js linter
 			},
