2b13a0c004bd7b3d927c012c93e404d445836ee8
waxcoin <waxcoin@hotmail.com>
2024-03-02 14:11:40 +0100

    Removed unused aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,14 +7,9 @@ alias vi nvim
 alias touch2 "~/.dotfiles/.config/fish/touch2 $1"
 alias chat "~/code/Term_ChatGPT/bin/chatGPT"
 alias code "cd ~/code"
-
 alias sshOxirs "ssh oxiris"
 alias sshconf "cat ~/.ssh/config"
-
 alias config "vi ~/.ssh/config"
-
-alias dot=dotfile
-
 alias gc="~/.GitSpeak/bin/GitSpeak"
 
 
@@ -37,12 +32,7 @@ export SHELL=/opt/local/bin/fish
 export NVM_DIR=~/.nvm
 set PATH $PATH ~/code/Go/GoHotReload/
 set PATH $PATH /Users/wax/.GitSpeak/bin
-
-# function create_new_window
-#     read query
-#
-#     tmux new-window -n $query
-# end
+set PATH $PATH /Users/wax/.cargo/bin
 
 function tmux-sessionizer
     sh ~/.dotfiles/.config/bin/tmux-sessionizer
@@ -52,100 +42,6 @@ bind \cf tmux-sessionizer
 bind \cy tmux-windows
 bind \cn create_new_window
 bind \ch tmux-menu
-
-alias 42="cd ~/code/42/$1"
-alias sfish "source ~/.dotfiles/.config/fish/config.fish"
-alias stmux "source ~/.dotfiles/.tmux.conf"
-alias vig "vi +G"
-alias vi nvim
-alias touch2 "~/.dotfiles/.config/fish/touch2 $1"
-alias chat "~/code/Term_ChatGPT/bin/chatGPT"
-alias code "cd ~/code"
-
-alias clearCache "rm -rf ~/.local/state/nvim/swap/*"
-
-
-alias ..="cd .."
-alias ...="cd ../.."
-alias ....="cd ../../.."
-alias .....="cd ../../../.."
-
-export VISUAL=/usr/local/bin/nvim
-export EDITOR=/usr/local/bin/nvim
-export SHELL=/opt/local/bin/fish
-export NVM_DIR=~/.nvm
-
-
-
-# set -g GOPATH $HOME/go
-# set -gx PATH $GOPATH/bin $PATH
-
-set PATH $PATH ~/code/Go/GoHotReload/
-set PATH $PATH /Users/wax/.cargo/bin
-
-function dotfile
-    gum style \
-        --border normal \
-        --margin 1 \
-        --border-foreground "$GIT_COLOR" \
-        " Dotfile Manager "
-    set choose (gum choose --no-limit "Nvim" "Fish" "Config" "Plugin" "Lsp" "HotConfig")
-    switch $choose
-        case Nvim
-            cd ~/.dotfiles/.config/nvim
-        case Fish
-            cd ~/.dotfiles/.config/fish
-        case Config
-            cd ~/.dotfiles/.config/
-        case Plugin
-            vi ~/.dotfiles/.config/nvim/lua/plugin.lua
-        case Lsp
-            cd ~/.dotfiles/.config/nvim/lua/lsp/
-        case HotConfig
-            vi ~/code/Go/GoHotReload/config/config.yml
-    end
-end
-
-function export_env_vars
-    for line in (cat .env)
-        if test (count (string split "=" $line)) -eq 2
-            set -l key_value (string split "=" $line)
-            set -gx $key_value[1] $key_value[2]
-        end
-    end
-end
-
-function dotfile
-    gum style \
-        --border normal \
-        --margin 1 \
-        --border-foreground "$GIT_COLOR" \
-        " Dotfile Manager "
-    set choose (gum choose --no-limit "Nvim" "Fish" "Config" "Plugin" "Lsp" "HotConfig")
-    switch $choose
-        case Nvim
-            cd ~/.dotfiles/.config/nvim
-        case Fish
-            cd ~/.dotfiles/.config/fish
-        case Config
-            cd ~/.dotfiles/.config/
-        case Plugin
-            vi ~/.dotfiles/.config/nvim/lua/plugin.lua
-        case Lsp
-            cd ~/.dotfiles/.config/nvim/lua/lsp/
-        case HotConfig
-            vi ~/code/Go/GoHotReload/config/config.yml
-    end
-end
-
-function export_env_vars
-    for line in (cat .env)
-        if test (count (string split "=" $line)) -eq 2
-            set -l key_value (string split "=" $line)
-            set -gx $key_value[1] $key_value[2]
-        end
-    end
-end
 
 # set theme_color_scheme solarized
 set -U FZF_COMPLETE 2
