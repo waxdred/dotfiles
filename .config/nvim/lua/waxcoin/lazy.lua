f2cdefaae02f374645877029b48647b16712f161
waxcoin <waxcoin@hotmail.com>
2024-09-30 15:02:20 +0200

    Update: directory files

--- .config/nvim/lua/waxcoin/lazy.lua
+++ .config/nvim/lua/waxcoin/lazy.lua
@@ -1,28 +1,33 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.g.mapleader = " "
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
-  { import = "waxcoin.plugins" },
-  { import = "waxcoin.plugins.lsp" },
+	{ import = "waxcoin.plugins" },
+	{ import = "waxcoin.plugins.AI" },
+	{ import = "waxcoin.plugins.completion" },
+	{ import = "waxcoin.plugins.format" },
+	{ import = "waxcoin.plugins.lsp" },
+	{ import = "waxcoin.plugins.style" },
+	{ import = "waxcoin.plugins.tools" },
 }, {
-  install = {
-    colorscheme = { "catppuccin-macchiato" },
-  },
-  checker = {
-    enable = true,
-    notify = false,
-  },
-  changet_detection = {
-    notify = false,
-  },
+	install = {
+		colorscheme = { "catppuccin-macchiato" },
+	},
+	checker = {
+		enable = true,
+		notify = false,
+	},
+	changet_detection = {
+		notify = false,
+	},
 })
