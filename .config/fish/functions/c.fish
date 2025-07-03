31425b185b619fdbdd1d358951b9a6a5f5bfc5b8
Jonathan Milhas <jonathan.milhas@gmail.com>
2025-07-03 15:17:33 +0200

    Add c.fish function for quick navigation to session directory

---
+++ .config/fish/functions/c.fish
@@ -0,0 +1,12 @@
+function c --description "Change to the last tmux session directory"
+    if test -f $HOME/.config/session
+        set -l session_path (cat $HOME/.config/session)
+        if test -d $session_path
+            cd $session_path
+        else
+            echo "Session path does not exist: $session_path"
+        end
+    else
+        echo "No session file found at $HOME/.config/session"
+    end
+end
