44d559dbbb200c90b220aac5131c7adca262a4d2
jonathan.milhas <jmilhas@protonmail.com>
2025-09-04 09:35:45 +0000

    Update installation linux fix erreur tmux config for linux

--- install.sh
+++ install.sh
@@ -10,13 +10,23 @@ Echo() {
     echo -e "\033[1;32m$1\033[0m"
 }
 
-if [[ "$unameOut" == *"Linux"* && "$unameOut" == *"arch"* ]]; then
-    echo "Installing dependencies for hyprland on arch linux..."
+tmux() {
+    Echo "Installing tmux..."
+    sudo pacman -S --noconfirm tmux
+    Echo "Installing tmux plugin manager..."
+    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+    Echo "link ../.tmux.conf to ~/.tmux.conf"
+    ln -s $(pwd)/.tmux.conf ~/.tmux.conf
+}
 
-    Echo "Updating system and installing fish shell..."
-    sudo pacman -Syu --noconfirm fish
+fish() {
+    Echo "Installing fish shell..."
+    sudo pacman -S --noconfirm fish
     Echo "Setup fish shell as default shell..."
     chsh -s /usr/bin/fish
+}
+
+hyprland() {
     Echo "Installing dependencies for hyprland..."
     sudo pacman -S --noconfirm \
         hyprland \
@@ -33,8 +43,10 @@ if [[ "$unameOut" == *"Linux"* && "$unameOut" == *"arch"* ]]; then
         thunar-volman \
         tumbler \
         ffmpegthumbnailer \
-        openssh \
+        openssh
+}
 
+nvim() {
     Echo "Installing nvim and other tools..."
 
     sudo pacman -S --noconfirm nvim \
@@ -43,11 +55,44 @@ if [[ "$unameOut" == *"Linux"* && "$unameOut" == *"arch"* ]]; then
         lua5 \
         luarocks \
         bluez-utils
+}
+
+link() {
     Echo "Link ../.config/ to ~/.config"
     for item in $(pwd)/.config/*; do
       Echo "Linking $item to ~/.config/"
       ln -s "$item" ~/.config/
     done
+}
+
+if [[ "$unameOut" == *"Linux"* && "$unameOut" == *"arch"* ]]; then
+    echo "Installing dependencies for hyprland on arch linux..."
+    ## switch case with first arg or run all if no arg
+    case $1 in
+      tmux)
+          tmux
+          ;;
+      fish)
+          fish
+          ;;
+      hyprland)
+          hyprland
+          ;;
+      nvim)
+          nvim
+          ;;
+      link)
+          link
+          ;;
+      *)
+          tmux
+          fish
+          hyprland
+          nvim
+          link
+          ;;
+    esac
+
 else
     Echo "Not arch linux, skipping dependencies installation..."
     Echo "Link .config/ to ~/.config"
