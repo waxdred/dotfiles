2166140a5c1a82076addf56b36949ef856384127
jonathan.milhas <jmilhas@protonmail.com>
2025-09-10 21:40:45 +0200

    update tmux coulor

--- .config/tmux/get_ip.sh
+++ .config/tmux/get_ip.sh
@@ -2,4 +2,4 @@
 
 ip=$(ip a | grep inet | grep \/24 | grep dynamic | awk '{print $2}' | cut -d'/' -f1)
 
-echo "#[fg=cyan] #[bg=cyan, fg=black]󰩟 ${ip}#[bg=#24273A, fg=cyan]"
+echo "#[fg=cyan] #[bg=cyan, fg=black]󰩟 ${ip}#[bg=default, fg=cyan]"
--- .config/tmux/tmux_gitbranch.sh
+++ .config/tmux/tmux_gitbranch.sh
@@ -10,7 +10,7 @@ get_git_branch() {
         branch=$(git branch --show-current)
         profile=$(git config --get user.name)
         if git status --porcelain | grep 'M'; then
-            echo "#[fg=red] #[bg=red, fg=black] ${profile}:${branch}#[bg=#24273A, fg=red]"
+            echo "#[fg=red] #[bg=red, fg=black] ${profile}:${branch}#[bg=default, fg=red]"
         else
             echo "#[fg=#A96FF4] #[bg=#A96FF4, fg=black] ${profile}:${branch}#[bg=#24273A, fg=#A96FF4]"
         fi
